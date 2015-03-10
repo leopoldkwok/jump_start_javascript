23afb8c583e868c7a1a6c76474eddc9d0f66a802
leopoldkwok <leopoldkwok@gmail.com>
2015-03-10 23:57:35 +0000

    project

---
+++ 04_objects_and_functions/projects-1.js
@@ -0,0 +1,6 @@
+
+Now that we have functions and objects figured out, we can start doing some fancy stuff with our task manager 
+project. Instead of a simple array of tasks, we can store richer information by using an array of objects. 
+We can also write some functions to perform operations for us. Let’s begin by wrapping each task in an object:
+
+
