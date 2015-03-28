6981720475e40e9fc4db20bc42f8c7e5b15db3fe
leopoldkwok <leopoldkwok@gmail.com>
2015-03-28 23:58:16 +0000

    for loop

--- 05_loops_and_jumps/do_while.js
+++ 05_loops_and_jumps/do_while.js
@@ -6,3 +6,31 @@ do {
 } while(expression);
 
 
+do {
+  console.log('This will print at least once');
+}
+while (false);
+
+We use while (false) to emphasize that the while check terminates the loop. Nonetheless, 
+there will still be a single print to the console. With a slight modifica- tion, our while example 
+can be rewritten as its do ... while equivalent:
+
+var tasksToDo = 3;
+
+do {
+	console.log('There are ' + tasksToDo + ' tasks to do');
+	tasksToDo--;
+}
+while (tasksToDo > 0);
+
+This code will result in exactly the same console output. For many problems, it is possible to use 
+either a while or a do ... while, so the question becomes which one to choose. 
+We would recommend while as the default choice, only using do ... while on those occasions when 
+a statement has to be executed at least once.
+
+
+
+
+
+
+
---
+++ 05_loops_and_jumps/for_loop.js
@@ -0,0 +1,11 @@
+The for loop is used to run a code block a number of times, and is often referred
+to as a counting loop. It takes this general form:
+
+or (initialization; condition; end-expression ) {
+  statement
+}
+
+for(var i =0; i < 3; i++) {
+	console.log(i);
+}
+
